7c0efcaa0415089e74a44a913bf68238b4729f7f
bugzhang <flyhighdream@qq.com>
2017-01-06 15:40:12 +0800

    no message

---
+++ indexOf.js
@@ -0,0 +1,10 @@
+function indexOf(arr, item) {
+    for(i=0;i<=arr.length;i++){
+        if(arr[i]==item){
+                      return i;
+        }
+            }
+            return -1;
+        }
+
+indexOf([ 1, 2, 3, 4 ], 3)
